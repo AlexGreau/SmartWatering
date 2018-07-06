e6f5ee8b140226095dd3b6199c4e9af2dff550a2
GraceBK <iris.b1991@gmail.com>
2018-07-06 10:07:21 +0200

    Init test code Arduino

---
+++ Arduino/stage_main/stage_main.ino
@@ -0,0 +1,49 @@
+#include <dht11.h>
+#include "variable.h"
+
+
+int getMoisture(int moisture_sensor) {
+  return analogRead(moisture_sensor);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pompe1, OUTPUT);
+  while(!Serial) {
+    // Attendre le port serie pour se connecter. Need for native USB (LEONARDO)
+  }
+}
+
+void loop() {
+  DHT11.read(DHT11PIN);
+
+
+  sensor_humidite1 = analogRead(moisture_sensor1);
+  sensor_humidite2 = analogRead(moisture_sensor2);
+  sensor_humidite3 = analogRead(moisture_sensor3);
+
+  Serial.print("Humidité (%): ");
+  Serial.print((float)DHT11.humidity, 2);
+  Serial.print("\t");
+  Serial.print("Température (°C): ");
+  Serial.println((float)DHT11.temperature, 2);
+  Serial.println("--------------------------------------------------");
+  Serial.print("Humidité 1 : ");
+  Serial.print(sensor_humidite1);
+  Serial.println(" % ");
+  Serial.print("Humidité 2 : ");
+  Serial.print(sensor_humidite2);
+  Serial.println(" % ");
+  Serial.print("Humidité 3 : ");
+  Serial.print(sensor_humidite3);
+  Serial.println(" % ");
+  Serial.println("--------------------------------------------------");
+
+  if (sensor_humidite1 > 10) {
+    digitalWrite(pompe1, HIGH);
+  } else {
+    digitalWrite(pompe1, LOW);
+  }
+
+  delay(1000);
+}
---
+++ Arduino/stage_main/variable.h
@@ -0,0 +1,66 @@
+#define DHT11PIN 2
+
+/**
+ * Module capteur d'Humidite
+ */
+const int moisture_sensor1 = A0;
+const int moisture_sensor2 = A1;
+const int moisture_sensor3 = A2;
+const int moisture_sensor4 = A3;
+int sensor_humidite1 = 0;
+int sensor_humidite2 = 0;
+int sensor_humidite3 = 0;
+int sensor_humidite4 = 0;
+
+/**
+ * Pin de gestion des pompes
+ * 7, 6, 5 et 4 correspondent au Relay utilise
+ */
+int pompe1 = 7;
+int pompe2 = 6;
+int pompe3 = 5;
+int pompe4 = 4;
+
+/**
+ * Module utilise pour le capteur de temperature et d'humidite
+ * Le capteur DHT11 est capable de mesurer :
+ * - des temperatures de 0 a + 50°C avec une precision de +/- 2°C
+ * - des taux d'humidite relative de 20 a 80% avec une precision de +/- 5°C
+ * une mesure peut etre realisee toutes les secondes
+ *
+ * Alimentation : 3.3 a 5 Volts
+
+ Humidite (relative %)      20 ~ 80%
+ Précision (humidite)       +/-5%
+
+ Temperature                0 ~ +50°C
+ Précision (temperature)    +/-2°C
+
+ Frequence mesure max       1Hz (1 mesure par seconde)
+ Tension d'Alimentation     3 ~ 5 Volts
+ Stabilite a long terme     +/- 1% par an
+ */
+dht11 DHT11_1;
+dht11 DHT11_2;
+dht11 DHT11_3;
+dht11 DHT11_4;
+
+
+
+
+
+
+
+struct Zone {
+  int *tabPompe;
+  int *tabMoisture;
+  int *tabTemperature;
+  int nbPompe;
+  int nbMoisture;
+  int nbTemperature;
+};
+
+struct POMPE {
+  int id;
+  bool start;
+};
