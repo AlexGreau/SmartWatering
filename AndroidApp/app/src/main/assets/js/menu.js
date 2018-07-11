5f6e1ca097aa0dd2bf8ef004c69e86dd580f1b69
AlexGreau <alexgreau@live.fr>
2018-07-11 17:59:43 +0200

    buttons good on my screen

--- AndroidApp/app/src/main/assets/js/menu.js
+++ AndroidApp/app/src/main/assets/js/menu.js
@@ -6,7 +6,7 @@ function setListenersMenuButtons() {
     var menuBtn = document.getElementById('menuBtn');
 
     // Get the <span> element that closes the modal
-    //var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close")[0];
 
 
     // When the user clicks on the button, open the modal
@@ -15,13 +15,13 @@ function setListenersMenuButtons() {
         modal.style.display = "block";
     }
 
-    /*
+    
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         menuBtn.classList.toggle("change");
         modal.style.display = "none";
     }
-    */
+    
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
