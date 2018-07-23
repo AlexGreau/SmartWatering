70f4c8f36b8cdcc538c3dcac35c7c1514ef3cfb2
GraceBK <iris.b1991@gmail.com>
2018-07-23 13:51:34 +0200

    Creation d'un compte

--- server/serveur.js
+++ server/serveur.js
@@ -1,25 +1,110 @@
-var http = require('http');
-var url = require('url');
-var mongoClient = require('mongodb');
+var http        = require('http');
+var url         = require('url');
+var express     = require('express');
+var mongoClient = require('mongodb').MongoClient;
+var mongoose    = require('mongoose');
+var bodyParser = require('body-parser');
+var urlbd      = "mongodb://localhost/smartwatering";
 
-var nouveauVisiteur = function (req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/html"}); // Code reponse 200 signifie "OK" pas d'erreur
-    if (page == '/') {
-        res.write('<strong>Salut tout le monde !</strong>');
-    } else if (page == '/test') {
-        res.write('None');
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+var pageHome = function (req, res) {
+    res.render('pages/index');
+}
+
+var signup = function (req, res) {
+    var mail = req.body.email;
+    var pass = req.body.password;
+
+    var data = {
+        "email": mail,
+        "password": pass,
+        "localite": req.body.bp
     }
-    res.end();
+
+    mongoClient.connect(urlbd, function (error, db) {
+        db.db('smartwatering').collection('user').insertOne(data, function (err, result) {
+            if (err) {
+                console.log('Insert failed', err);
+            } else {
+                console.log('Insert successful', result);
+                res.render('pages/mypage');
+            }
+        });
+    });
 }
 
-var serverClose = function () {
-    console.log('Bye bye !');
+var signin = function (req, res) {
+    var mail = req.body.email;
+    var pass = req.body.password;
+
+    var data = {
+        "email": mail,
+        "password": pass
+    }
+
+    mongoClient.connect(urlbd, function (error, db) {
+        db.db('smartwatering').collection('user').find({email: mail, password: pass}).toArray(function (err, result) {
+            if (err) {
+                console.log('Find failed', err);
+            } else if (result.length > 0) {
+                console.log('Find OK', result);
+                res.render('pages/mypage');
+            } else {
+                console.log('Find OK', result);
+                // TODO pas Enregistré ou mot de passe oublié
+            }
+        });
+    });
 }
 
-var server = http.createServer(nouveauVisiteur);
+app.get('/', pageHome);
 
-server.on('close', serverClose);    // On ecoute l'evenement close
-server.listen(8080);    // On Demarre le serveur
-server.close(); // Arrete le serveur. Declenche l'evenement close
+app.post('/adduser', signup);
+
+app.post('/login', signin);
+
+var connectDB = function (error, db) {
+    if (error) {
+        return console.error('Connction DB failed', error);;
+    }
+    console.log("Connection DB successful", urlbd);
+
+    db.db('smartwatering').collection('user').find().toArray(function (err, result) {
+        if (err) {
+            console.log('Find failed', err);
+        } else {
+            console.log('Find OK', result);
+        }
+    });
+
+    db.close();
+}
+
+
+
+
+
+
+mongoClient.connect(urlbd, connectDB);
+
+
+
+
+
+
+
+// Gestion des erreurs 404
+app.use(function (req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(404).render('pages/404');
+});
+app.listen(8080, function () {
+    console.log("Connected at port 8080");
+});
