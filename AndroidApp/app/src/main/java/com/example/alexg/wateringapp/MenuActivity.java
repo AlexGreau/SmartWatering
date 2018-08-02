4397123b2206387591720c8206b91c0c20c65503
AlexGreau <alexgreau@live.fr>
2018-08-02 11:46:43 +0200

    button launches blockly

--- AndroidApp/app/src/main/java/com/example/alexg/wateringapp/MenuActivity.java
+++ AndroidApp/app/src/main/java/com/example/alexg/wateringapp/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexg.wateringapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +14,7 @@ public class MenuActivity extends AppCompatActivity {
 
     public void onProgram(View view){
         // when program button is pressed in menu
-
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
