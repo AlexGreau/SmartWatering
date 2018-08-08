64e018e615b294d160abac02949c84ea3e0f0a1c
GraceBK <iris.b1991@gmail.com>
2018-08-08 12:02:12 +0200

    Creation de RegisterActivity

--- AndroidApp/app/src/main/java/com/stage/wateringapp/RegisterActivity.java
+++ AndroidApp/app/src/main/java/com/stage/wateringapp/RegisterActivity.java
@@ -1,27 +1,38 @@
 package com.stage.wateringapp;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     private EditText addr_mail;
     private EditText pass;
-    private CardView signin_btn;
+    private AutoCompleteTextView city;
+    private CardView signup_btn;
+
+    ArrayAdapter<String> adapter;
+
+    private static final String[] CITY = new String[] {
+            "Nice, Fr", "Paris, Fr", "Nantes, Fr", "Lyon, fr"
+    };
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         initView();
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, CITY);
 
         addr_mail.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -39,25 +50,28 @@ public class LoginActivity extends AppCompatActivity {
                 return false;
             }
         });
+        city.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.setFocusable(true);
+                view.setFocusableInTouchMode(true);
+                return false;
+            }
+        });
+        city.setAdapter(adapter);
 
-        signin_btn.setOnClickListener(new View.OnClickListener() {
+        signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // ToDo : Action connection
             }
         });
-
-
     }
 
     public void initView() {
-        addr_mail = findViewById(R.id.et_username);
-        pass = findViewById(R.id.et_password);
-        signin_btn = findViewById(R.id.cv_signin);
-    }
-
-    public void onCreateNewCompte(View view) {
-        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-        startActivity(intent);
+        addr_mail = findViewById(R.id.et_register_mail);
+        pass = findViewById(R.id.et_register_password);
+        city = findViewById(R.id.actv_register_city);
+        signup_btn = findViewById(R.id.cv_register);
     }
 }
