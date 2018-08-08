59b996bb4dbd70e3d8867a8f1e5d5e3392db0ae3
GraceBK <iris.b1991@gmail.com>
2018-08-08 11:01:40 +0200

    Creation de LoginActivity

--- AndroidApp/app/src/main/java/com/stage/wateringapp/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/stage/wateringapp/LoginActivity.java
@@ -1,15 +1,57 @@
 package com.stage.wateringapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import com.stage.wateringapp.R;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText addr_mail;
+    private EditText pass;
+    private CardView signin_btn;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+
+        addr_mail.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.setFocusable(true);
+                view.setFocusableInTouchMode(true);
+                return false;
+            }
+        });
+        pass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.setFocusable(true);
+                view.setFocusableInTouchMode(true);
+                return false;
+            }
+        });
+
+        signin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ToDo : Action connection
+            }
+        });
+
+
+    }
+
+    public void initView() {
+        addr_mail = findViewById(R.id.et_username);
+        pass = findViewById(R.id.et_password);
+        signin_btn = findViewById(R.id.cv_signin);
     }
 }
